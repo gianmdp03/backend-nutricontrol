2464a2d96caa3bb4d87c211448cd975e213ad545
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-04-26 11:32:40 -0300

    Update ScheduleExceptionMapper.java

--- src/main/java/com/erick/nutricontrol/mapper/ScheduleExceptionMapper.java
+++ src/main/java/com/erick/nutricontrol/mapper/ScheduleExceptionMapper.java
@@ -1,7 +1,19 @@
 package com.erick.nutricontrol.mapper;
 
-import org.mapstruct.Mapper;
+
+import com.erick.nutricontrol.dto.scheduleException.ScheduleExceptionDetailDTO;
+import com.erick.nutricontrol.dto.scheduleException.ScheduleExceptionRequestDTO;
+import com.erick.nutricontrol.dto.scheduleException.ScheduleExceptionUpdateDTO;
+import com.erick.nutricontrol.model.ScheduleException;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public abstract class ScheduleExceptionMapper {
+    @Mapping(target = "id", ignore = true)
+    public abstract ScheduleException toEntity(ScheduleExceptionRequestDTO dto);
+    public abstract ScheduleExceptionDetailDTO toDetailDTO(ScheduleException entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    public abstract void updateEntityFromDto(ScheduleExceptionUpdateDTO dto, @MappingTarget ScheduleException entity);
 }
