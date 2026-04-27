810a6063ad74ea170c17de300f794f193ba9ff37
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-04-27 14:47:25 -0300

    Update ScheduleExceptionRepository.java

--- src/main/java/com/erick/nutricontrol/repository/ScheduleExceptionRepository.java
+++ src/main/java/com/erick/nutricontrol/repository/ScheduleExceptionRepository.java
@@ -3,5 +3,8 @@ package com.erick.nutricontrol.repository;
 import com.erick.nutricontrol.model.ScheduleException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface ScheduleExceptionRepository extends JpaRepository<ScheduleException, Long> {
+    boolean existsByDate(LocalDate date);
 }
