813e612d8e51d833ceb3126ad022a7ea2d97311b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-04-27 14:47:13 -0300

    Update Appointment.java

--- src/main/java/com/erick/nutricontrol/model/Appointment.java
+++ src/main/java/com/erick/nutricontrol/model/Appointment.java
@@ -1,9 +1,14 @@
 package com.erick.nutricontrol.model;
 
+import com.erick.nutricontrol._enum.AppointmentStatus;
+import com.erick.nutricontrol.security.user.model.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Getter
@@ -14,4 +19,31 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Long adminId;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus appointmentStatus;
+
+    public Appointment(LocalDate date, LocalTime startTime, LocalTime endTime, AppointmentStatus appointmentStatus) {
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.appointmentStatus = appointmentStatus;
+    }
 }
