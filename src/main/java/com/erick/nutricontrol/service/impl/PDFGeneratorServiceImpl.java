15b329d78ebbcc8c8fe6505a9574b0feb0dd30ec
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-05-11 15:22:46 -0300

    Add font file

--- src/main/java/com/erick/nutricontrol/service/impl/PDFGeneratorServiceImpl.java
+++ src/main/java/com/erick/nutricontrol/service/impl/PDFGeneratorServiceImpl.java
@@ -1,6 +1,9 @@
 package com.erick.nutricontrol.service.impl;
 
 import com.erick.nutricontrol.service.PDFGeneratorService;
+import lombok.RequiredArgsConstructor;
+import org.openpdf.text.pdf.BaseFont;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -8,13 +11,9 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 import java.io.ByteArrayOutputStream;
 
 @Service
+@RequiredArgsConstructor
 public class PDFGeneratorServiceImpl implements PDFGeneratorService {
-
     private final TemplateEngine templateEngine;
-
-    public PDFGeneratorServiceImpl(TemplateEngine templateEngine) {
-        this.templateEngine = templateEngine;
-    }
 
     @Override
     public byte[] generateAppointmentReceipt(String patientName, String date, String time, String doctorName) throws Exception {
@@ -28,6 +27,14 @@ public class PDFGeneratorServiceImpl implements PDFGeneratorService {
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ITextRenderer renderer = new ITextRenderer();
+
+        ClassPathResource fontResource = new ClassPathResource("fonts/Roboto-Regular.ttf");
+        renderer.getFontResolver().addFont(
+                fontResource.getURL().toString(),
+                BaseFont.IDENTITY_H,
+                BaseFont.EMBEDDED
+        );
+
         renderer.setDocumentFromString(htmlContent);
         renderer.layout();
         renderer.createPDF(outputStream);
